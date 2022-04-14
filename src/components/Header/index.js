18b823109de397f23c830cb955a9ad484f509531
WagnerNasla <wagnerjr.nascimento10@gmail.com>
2022-04-14 03:57:27 -0300

    Alteracao de header e insercao de icon com funcionalidade de informar a quantidade de filmes nos favoritos, porem nao esta dinamico verificar uma forma de observar a alteracao do JSON no localStorage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,25 @@
-import './header.css';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import image from '../../Assets/bell_icon2.svg';
+import './header.css';
 
 export default function Header() {
+    const [count, setCount] = useState([]);
+
+    // Use Effect para ciclo de vida e acessar localStorage e useState por conta dos estados
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem('filmes');
+        setCount(JSON.parse(minhaLista).length || []);
+        let teste = count;
+    }, [])
+
     return(
         <header>
             <Link className="logo" to="/">Rengan Filmes</Link>
-            <Link className="favoritos" to="/favoritos">Favoritos</Link>
+            <Link className="icon-wrapper" to="/favoritos" data-number= {count}>
+                <img src={image} alt="" className="bell-icon"/>
+            </Link>
         </header>
     )
 }
